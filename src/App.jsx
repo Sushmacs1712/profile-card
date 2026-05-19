b4e973905df001b7844e07fed1d1d1ba6fcd18ed
Sushmacs1712 <saraganacharisushma@gmail.com>
2026-05-19 17:10:56 +0530

    create first React navbar component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div>
+      <h1>Sushma Portfolio</h1>
+      <p>Frontend Developer</p>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Sushma</h2>
+    </nav>
+  );
+}
+
+export default Navbar;

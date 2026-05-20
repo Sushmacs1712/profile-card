448851f3b377abc38f57a51149a6782fea62abf8
Sushmacs1712 <saraganacharisushma@gmail.com>
2026-05-20 13:29:39 +0530

    Improve navbar styling and routing setup

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ display: "flex", justifyContent: "space-between" }}>
-      <h2>Sushma</h2>
+    <nav className="navbar">
+      <h2 className="logo">Sushma</h2>
 
-      <div style={{ display: "flex", gap: "20px" }}>
+      <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/skills">Skills</Link>
